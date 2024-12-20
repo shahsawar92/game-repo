cc2fd28d8e9f3e67d6df7739a3e8d09e58f8e9f0
shahsawar92 <shahsawar92@gmail.com>
2024-12-20 15:04:59 +0500

    push

--- components/navbar/index.jsx
+++ components/navbar/index.jsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 import React from "react";
 import Logo from "@/public/assets/logo.png";
 import { GradientBtn } from "..";
-import { useRouter } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import { setCookie } from "cookies-next";
 import {
   IS_LOGGED_IN,
@@ -15,23 +15,27 @@ import {
 
 export const Navbar = () => {
   const router = useRouter();
-
+  const pathname = usePathname();
+  const isLoginPage = pathname === "/";
+  console.log("pathname: ", pathname);
   return (
     <div className='w-[80%] mx-auto flex justify-center gap-6 lg:justify-between flex-wrap pb-10'>
       <Image alt='logo' src={Logo} height={1} width={150} />
       <div className='flex gap-2 lg:gap-5'>
-        <GradientBtn
-          onClick={() => {
-            setCookie(REMEMBER_ME, false);
-            localStorage.removeItem(REMEMBER_ME);
-            localStorage.removeItem(USER_TOKEN);
-            localStorage.removeItem(USER_DATA);
-            setCookie(IS_LOGGED_IN, false);
-            console.log("redirecting...");
-            router.push("/");
-          }}
-          title={"Logout"}
-        />
+        {!isLoginPage && (
+          <GradientBtn
+            onClick={() => {
+              setCookie(REMEMBER_ME, false);
+              localStorage.removeItem(REMEMBER_ME);
+              localStorage.removeItem(USER_TOKEN);
+              localStorage.removeItem(USER_DATA);
+              setCookie(IS_LOGGED_IN, false);
+              console.log("redirecting...");
+              router.push("/");
+            }}
+            title={"Logout"}
+          />
+        )}
       </div>
     </div>
   );
