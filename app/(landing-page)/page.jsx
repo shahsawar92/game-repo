e71af7839968b61d17dde1b0b0d3812f0d54c564
shahsawar92 <shahsawar92@gmail.com>
2025-01-29 12:08:10 +0500

    added forget password

--- app/(landing-page)/page.jsx
+++ app/(landing-page)/page.jsx
@@ -1,20 +1,20 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
-import LoginBorder from "@/public/assets/login-border.png";
+import React, { useState } from "react";
 import Image from "next/image";
-import { set, useForm } from "react-hook-form";
-import ButtonBorder from "@/public/assets/button-border.png";
+import { useForm } from "react-hook-form";
 import { useRouter } from "next/navigation";
 import Swal from "sweetalert2";
 import { post } from "@/utils/api";
-import { setCookie } from "cookies-next"; // Client-side cookie setting
+import { setCookie } from "cookies-next";
 import {
   IS_LOGGED_IN,
   REMEMBER_ME,
   USER_DATA,
   USER_TOKEN,
 } from "@/components/constants/constants";
+import LoginBorder from "@/public/assets/login-border.png";
+import ButtonBorder from "@/public/assets/button-border.png";
 
 export default function Login() {
   const {
@@ -28,35 +28,21 @@ export default function Login() {
   const onSubmit = async (data) => {
     try {
       const response = await post("/api/login", data);
-      console.log("Response: ", response);
       if (response.success) {
-        if (typeof window !== "undefined") {
-          localStorage.setItem(USER_DATA, JSON.stringify(response.data));
-          localStorage.setItem(USER_TOKEN, response.data.token);
-          setCookie(IS_LOGGED_IN, true);
-
-          if (rememberMe) {
-            localStorage.setItem(REMEMBER_ME, true);
-            setCookie(REMEMBER_ME, true, { maxAge: 30 * 24 * 60 * 60 });
-          } else {
-            localStorage.setItem(USER_DATA, JSON.stringify(response.data));
-            localStorage.setItem(USER_TOKEN, response.data.token);
-            localStorage.removeItem(REMEMBER_ME);
-            setCookie(REMEMBER_ME, false);
-          }
-        }
-
+        localStorage.setItem(USER_DATA, JSON.stringify(response.data));
+        localStorage.setItem(USER_TOKEN, response.data.token);
+        setCookie(IS_LOGGED_IN, true);
+        if (rememberMe)
+          setCookie(REMEMBER_ME, true, { maxAge: 30 * 24 * 60 * 60 });
+        else localStorage.removeItem(REMEMBER_ME);
         Swal.fire("Success!", "Login successful!", "success");
-        if (response.data.user_type === 1) {
-          router.push("/dashboard");
-        } else {
-          router.push("/landing-page");
-        }
+        router.push(
+          response.data.user_type === 1 ? "/dashboard" : "/landing-page"
+        );
       } else {
         Swal.fire("Error!", response.message || "Login failed.", "error");
       }
     } catch (error) {
-      console.error("Error: ", error);
       Swal.fire(
         "Error!",
         error.response?.message || "An error occurred.",
@@ -65,20 +51,102 @@ export default function Login() {
     }
   };
 
+  const handleForgotPassword = async () => {
+    const { value: email } = await Swal.fire({
+      title: "Forgot Password?",
+      input: "email",
+      inputLabel: "Enter your email",
+      inputPlaceholder: "you@example.com",
+      showCancelButton: true,
+    });
+
+    if (email) {
+      try {
+        const response = await post("/api/forgot-password-code", { email });
+        if (response.success) {
+          await Swal.fire(
+            "Success!",
+            "A one-time code has been sent to your email.",
+            "success"
+          );
+          handleVerifyCode(email);
+        } else {
+          Swal.fire(
+            "Error!",
+            response.message || "Email does not exist.",
+            "error"
+          );
+        }
+      } catch (error) {
+        Swal.fire(
+          "Error!",
+          "An error occurred while sending the code.",
+          "error"
+        );
+      }
+    }
+  };
+
+  const handleVerifyCode = async (email) => {
+    const { value: formData } = await Swal.fire({
+      title: "Reset Password",
+      html:
+        '<input id="swal-input1" class="swal2-input" placeholder="Enter OTP" autocomplete="off">' +
+        '<input id="swal-input2" type="password" class="swal2-input" placeholder="New Password" autocomplete="new-password">' +
+        '<input id="swal-input3" type="password" class="swal2-input" placeholder="Confirm Password" autocomplete="new-password">',
+      focusConfirm: false,
+      showCancelButton: true,
+      allowOutsideClick: false,
+      preConfirm: () => {
+        return {
+          email: "shahsawarpro@gmail.com",
+          reset_code: document.getElementById("swal-input1").value,
+          password: document.getElementById("swal-input2").value,
+          password_confirmation: document.getElementById("swal-input3").value,
+        };
+      },
+    });
+    console.log("formData", formData);
+
+    if (formData) {
+      if (formData.newPassword !== formData.confirmPassword) {
+        Swal.fire("Error!", "Passwords do not match.", "error");
+        return;
+      }
+      try {
+        const response = await post("/api/verify-reset-code", formData);
+        console.log("response", response);
+        if (response.success) {
+          Swal.fire("Success!", "Password reset successfully.", "success");
+        } else {
+          Swal.fire("Error!", response.message || "Invalid code.", "error");
+        }
+      } catch (error) {
+        console.log("error", error);
+        const errorData = error.response?.data.data;
+        let errorMessages = errorData
+          ? Object.values(errorData).flat().join("\n") // Combine all error messages
+          : error.response?.data.message || "An error occurred.";
+
+        Swal.fire("Error!", errorMessages, "error");
+      }
+    }
+  };
+
   return (
     <div className='w-full lg:w-[80%] mx-auto flex flex-col gap-10 pb-10'>
-      <div className='font-extrabold text-4xl lg:text-[80px] text-center leading-none px-[20px] lg:px-[250px] rowdies-font'>
+      <div className='font-extrabold text-4xl lg:text-[80px] text-center'>
         Login
       </div>
       <div className='relative w-[90%] lg:w-[50%] mx-auto'>
         <Image
           src={LoginBorder}
           alt='login-border'
-          className='w-full lg:h-[350px]'
+          className='w-full h-[350px]'
         />
         <form
           onSubmit={handleSubmit(onSubmit)}
-          className='absolute inset-0 flex flex-col gap-5 lg:gap-10 justify-center items-center p-4'>
+          className='absolute inset-0 flex flex-col gap-3 lg:gap-5 justify-center items-center p-4'>
           <div className='flex flex-col gap-2 lg:gap-5 w-full'>
             <input
               id='email'
@@ -112,6 +180,7 @@ export default function Login() {
               Remember Me
             </label>
           </div>
+
           <button type='submit' className='relative w-full'>
             <Image
               src={ButtonBorder}
@@ -119,6 +188,11 @@ export default function Login() {
               className='lg:w-[50%] 2xl:w-[40%] mx-auto'
             />
           </button>
+          <div
+            className='text-white text-sm cursor-pointer'
+            onClick={handleForgotPassword}>
+            Forgot Password?
+          </div>
         </form>
       </div>
     </div>
